1f95b49b9773140101f4878d12b76d447278399d
mahmoud20212 <occee123@gmail.com>
2025-02-27 20:47:33 +0200

    first commit

---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Employer;
+use App\Models\Job;
+use PHPUnit\Framework\TestCase;
+
+class JobTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+    
+    public function test_belongs_to_an_employer(): void
+    {
+        $employer = Employer::factory()->create();
+        $job = Job::factory()->create([
+            "employer_id"=> $employer->id,
+        ]);
+
+        $this->assertTrue($job->employer->is($employer));
+    }
+    
+    public function test_can_have_tags(): void
+    {
+        $job = Job::factory()->create();
+        $job->tag('Frontend');
+
+        $this->assertCount(1, $job->tags);
+    }
+}
