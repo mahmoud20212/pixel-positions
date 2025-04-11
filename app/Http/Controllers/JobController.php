a8b5781667aaa3e516ee13378d032269b4b327d3
mahmoud20212 <occee123@gmail.com>
2025-04-11 07:50:16 +0300

    new update

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,10 @@ use App\Models\Job;
 use App\Http\Requests\StoreJobRequest;
 use App\Http\Requests\UpdateJobRequest;
 use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class JobController extends Controller
 {
@@ -14,10 +18,11 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::all()->groupBy('featured');
+        $jobs = Job::latest()->with(['employer', 'tags'])->get()->groupBy('featured');
+        
         return view("jobs.index", [
-            'featuredJobs' => $jobs[0],
-            'jobs' => $jobs[1],
+            'jobs' => $jobs[0],
+            'featuredJobs' => $jobs[1],
             'tags' => Tag::all(),
         ]);
     }
@@ -27,46 +32,34 @@ class JobController extends Controller
      */
     public function create()
     {
-        //
+        return view('jobs.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreJobRequest $request)
+    public function store(Request $request)
     {
-        //
-    }
+        $atrributes = $request->validate([
+            // 'employer_id' => ['required', 'exists:employers'],
+            'title' => ['required'],
+            'salary' => ['required'],
+            'location' => ['required'],
+            'schedule' => ['required', Rule::in(['Part Time', 'Full Time'])],
+            'url' => ['required', 'active_url'],
+            'tags' => ['nullable'],
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Job $job)
-    {
-        //
-    }
+        $atrributes['featured'] = $request->has('featured');
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Job $job)
-    {
-        //
-    }
+        $job = Auth::user()->employer->jobs()->create(Arr::except($atrributes, 'tags'));
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateJobRequest $request, Job $job)
-    {
-        //
-    }
+        if ($atrributes['tags'] ?? false) {
+            foreach (explode(',', $atrributes['tags']) as $tag) {
+                $job->tag($tag);
+            }
+        }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Job $job)
-    {
-        //
+        return redirect('/');
     }
 }
---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view("auth.login");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $atrributes = $request->validate([
+            "email" => ["required","email"],
+            "password" => ["required"],
+        ]);
+
+        if (!Auth::attempt($atrributes)) {
+            throw ValidationException::withMessages([
+                "email"=> "Sorry, those credentials do not match."
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
